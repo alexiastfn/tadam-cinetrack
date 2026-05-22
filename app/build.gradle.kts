f75944ccba1be0e7686fa46591b5d00cb794e9b5
alexiastfn <stefanalexia01@gmail.com>
2026-05-22 16:49:29 +0300

    Update Room version, upgrade compile and target SDK to 36, and move INTERNET permission declaration outside of the application tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ if (localPropertiesFile.exists()) {
 
 android {
     namespace = "com.example.cinetrack"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.cinetrack"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
